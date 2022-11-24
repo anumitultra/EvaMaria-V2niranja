cceeb3bf0317ab0d418bca2b8a1f965b2f144452
anumitultra <105001750+anumitultra@users.noreply.github.com>
2022-11-24 14:06:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/EvaMaria-V2 /EvaMaria-V2
+  git clone https://github.com/anumitultra/EvaMaria-V2niranja /EvaMaria-V2niranja
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria-V2
+  git clone $UPSTREAM_REPO /EvaMaria-V2niranja
 fi
-cd /EvaMaria-V2
+cd /EvaMaria-V2niranja
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
